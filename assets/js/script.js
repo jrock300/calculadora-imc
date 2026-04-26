72cdcf6eeecfcd0b35dd03aa2400ad0d79ebf197
jrock300 <jrock300@gmail.com>
2026-04-26 02:47:18 -0300

    feat: ajuste no escopo das variaveis da funcao lerDados - const

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,8 +21,8 @@ const limparCampos = () => {
 
 // Ler Dados
 const lerDados = () => {
-  uPeso = Number(peso.value.replace(",", "."));
-  uAlt = Number(altura.value.replace(",", "."));
+  const uPeso = Number(peso.value.replace(",", "."));
+  const uAlt = Number(altura.value.replace(",", "."));
   return { uPeso, uAlt };
 };
 
