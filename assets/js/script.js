65aaac8727659c5ede0c942239684584514f8015
jrock300 <jrock300@gmail.com>
2026-04-26 01:57:29 -0300

    feat: criacao da funcao valida campos

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,47 +26,61 @@ const lerDados = () => {
   return { uPeso, uAlt };
 };
 
+// Valida Dados
+const validaDados = (peso, altura) => {
+  if (!peso || isNaN(peso) || peso <= 0) {
+    descricaoImc.textContent = "Digite um peso válido.";
+    return false;
+  }
+
+  if (!altura || isNaN(altura) || altura <= 0) {
+    descricaoImc.textContent = "Digite um peso válido.";
+    return false;
+  }
+  return true;
+};
+
 // Calcula IMC
-const calculaIMC = (peso, altura) => {
+const calcularIMC = (peso, altura) => {
   const imc = peso / (altura * altura);
   return imc;
 };
 
-function calcularImc() {
-  const peso = document.querySelector("#peso").value;
-  const altura = document.querySelector("#altura").value;
-  const resultadoImc = document.querySelector(".valor-imc");
-  const descricaoImc = document.querySelector(".descricao-imc");
+// function calcularImc() {
+//   const peso = document.querySelector("#peso").value;
+//   const altura = document.querySelector("#altura").value;
+//   const resultadoImc = document.querySelector(".valor-imc");
+//   const descricaoImc = document.querySelector(".descricao-imc");
 
-  // O "toFixed(2)" faz com que sejam exibidas apenas 2 casas decimais apos a vírgula
-  const imc = (peso / (altura * altura)).toFixed(2);
+//   // O "toFixed(2)" faz com que sejam exibidas apenas 2 casas decimais apos a vírgula
+//   const imc = (peso / (altura * altura)).toFixed(2);
 
-  if (peso === "" || altura === "") {
-    // Exibe no HTML que o usuário deve preencher os camos corretamente
-    descricaoImc.innerHTML = "Preencha todos os campos, para obter seu IMC.";
-  } else if (imc < 18.5) {
-    // Exibe no HTML o resultado do IMC
-    resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
-    descricaoImc.innerHTML = "Você está abaixo do peso.";
-  } else if (imc >= 18.5 && imc <= 24.99) {
-    // Exibe no HTML o resultado do IMC
-    resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
-    descricaoImc.innerHTML = "Você está no seu peso ideal.";
-  } else if (imc >= 25.0 && imc <= 29.99) {
-    // Exibe no HTML o resultado do IMC
-    resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
-    descricaoImc.innerHTML = "Você está em sobrepeso 1.";
-  } else if (imc >= 30.0 && imc <= 39.99) {
-    // Exibe no HTML o resultado do IMC
-    resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
-    descricaoImc.innerHTML = "Você está em obesidade grau 2.";
-  } else if (imc > 40.0) {
-    // Exibe no HTML o resultado do IMC
-    resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
-    descricaoImc.innerHTML = "Você está em obesidade grau 3 ( Grave ).";
-  }
-}
+//   if (peso === "" || altura === "") {
+//     // Exibe no HTML que o usuário deve preencher os camos corretamente
+//     descricaoImc.innerHTML = "Preencha todos os campos, para obter seu IMC.";
+//   } else if (imc < 18.5) {
+//     // Exibe no HTML o resultado do IMC
+//     resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
+//     descricaoImc.innerHTML = "Você está abaixo do peso.";
+//   } else if (imc >= 18.5 && imc <= 24.99) {
+//     // Exibe no HTML o resultado do IMC
+//     resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
+//     descricaoImc.innerHTML = "Você está no seu peso ideal.";
+//   } else if (imc >= 25.0 && imc <= 29.99) {
+//     // Exibe no HTML o resultado do IMC
+//     resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
+//     descricaoImc.innerHTML = "Você está em sobrepeso 1.";
+//   } else if (imc >= 30.0 && imc <= 39.99) {
+//     // Exibe no HTML o resultado do IMC
+//     resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
+//     descricaoImc.innerHTML = "Você está em obesidade grau 2.";
+//   } else if (imc > 40.0) {
+//     // Exibe no HTML o resultado do IMC
+//     resultadoImc.innerHTML = `O seu IMC é de: ${imc}`;
+//     descricaoImc.innerHTML = "Você está em obesidade grau 3 ( Grave ).";
+//   }
+// }
 
 // Event listener dos botões
 btnLimpar.addEventListener("click", limparCampos);
-btnCalcular.addEventListener("click", calcularImc);
+btnCalcular.addEventListener("click", calcularIMC);
